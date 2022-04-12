b8b3a3b50c93b5b14d66a3be940250075e03396a
RillianDiello <rilliansf@gmail.com>
2022-04-12 14:17:08 -0400

    Wrappers

---
+++ Wrappers/PagedResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApplication1.Wrappers
+{
+    public class PagedResponse<T> : Response<T>
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public Uri FirstPage { get; set; }
+        public Uri LastPage { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalRecords { get; set; }
+        public Uri NextPage { get; set; }
+        public Uri PreviousPage { get; set; }
+        public PagedResponse(T data, int pageNumber, int pageSize, int totalPages)
+        {
+            this.PageNumber = pageNumber;
+            this.PageSize = pageSize;
+            this.TotalPages = totalPages;
+            this.Data = data;
+            this.Message = null;
+            this.Succeeded = true;
+            this.Errors = null;
+        }
+    }
+}
