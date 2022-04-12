b47f7f47f6c5444697e33b66d174721de3d7be05
RillianDiello <rilliansf@gmail.com>
2022-04-12 14:17:34 -0400

    Controllers

---
+++ Controllers/FileUpsController.cs
@@ -0,0 +1,77 @@
+﻿#nullable disable
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Services;
+using WebApplication1.Wrappers;
+
+namespace WebApplication1.Controllers
+{    
+    [Route("api/[controller]")]
+    [ApiController]   
+    public class FileUpsController : ControllerBase
+    {
+
+        private readonly FileServices fileServices;
+
+        public FileUpsController(FileServices fileService)
+        {
+            fileServices = fileService;
+        }
+
+
+        // GET: api/FileUps
+        [HttpGet]
+        // [Route("")]
+        public async Task<PagedResponse<IEnumerable<FileUp>>> GetFilesUp( int page = 1, int per_page = 1)
+        {
+            var data = await fileServices.GetFilesUp(page, per_page);
+            return data;
+        }
+
+        // GET: api/FileUps/5
+        [HttpGet("{id}")]
+        //// [Route("")]
+        public async Task<ActionResult<FileUp>> GetFileUp(int id, [FromServices] _DBContext context)
+        {
+            var fileUp = await context.FilesUp.FindAsync(id);
+
+            if (fileUp == null)
+            {
+                return NotFound();
+            }
+            return fileUp;
+        }
+
+        // DELETE: api/FileUps/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFileUp(int id, [FromServices] _DBContext context)
+        {
+            var fileUp = await context.FilesUp.FindAsync(id);
+            if (fileUp == null)
+            {
+                return NotFound();
+            }
+
+            context.FilesUp.Remove(fileUp);
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+             
+
+        //Upload file
+        [HttpPost("")]
+        public IActionResult Upload(IFormFile file, [FromServices] IWebHostEnvironment hostingEnvironment, [FromServices] _DBContext context)
+        {
+            string fileName = $"{hostingEnvironment.ContentRootPath}\\temp\\{file.FileName}"; 
+            using (FileStream fileStream = System.IO.File.Create(fileName))
+            {
+                file.CopyTo(fileStream);
+                fileStream.Flush();
+            }
+            fileServices.CreateFile(fileName, file.FileName);
+
+            return Ok();
+        }
+    }
+}
---
+++ Controllers/SamplesController.cs
@@ -0,0 +1,54 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+using WebApplication1.Services;
+using WebApplication1.Wrappers;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SamplesController : ControllerBase
+    {
+        private readonly SampleServices sampleServices;        
+
+        public SamplesController(SampleServices sampleService)
+        {
+            sampleServices = sampleService;
+        }
+
+        // GET: api/Samples/5        
+        [HttpGet("{fileUp}")]
+        public async Task<PagedResponse<IEnumerable<Sample>>> GetSamples(int fileUp, int page = 1, int per_page = 1)
+        {
+            var data = await sampleServices.GetSamplesByFilesUp(fileUp, page, per_page);
+            return data;           
+        }
+
+        [HttpGet("minSample/{fileUp}")]
+        public async Task<double> GetMinSamples(int fileUp)
+        {
+            return await sampleServices.MinValue(fileUp);
+        }
+
+        [HttpGet("maxSample/{fileUp}")]
+        public async Task<double> GetMaxSamples(int fileUp)
+        {
+            return await sampleServices.MaxValue(fileUp);
+        }
+
+
+        [HttpGet("range/{fileUp}")]
+        public async Task<List<Sample>> GetRange(int fileUp)
+        {
+            return await sampleServices.MostExpensive(fileUp);
+        }
+
+    }
+}
